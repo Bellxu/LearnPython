3d5f77202db3d95e1951a113ef7aac9615cfc349
徐晟恺 <xushengkai@jjsnt.com>
2024-03-11 18:19:31 +0800

    使用模板HTML

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -9,7 +9,7 @@
 
 
 # learning_logs/urls.py
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'learning_logs'
@@ -17,4 +17,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     # 主页
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
 ]
